6d3a9f817263d2de4ad2c226e3ae1207fc18a6ef
nikki9750 <76221172+tam1192@users.noreply.github.com>
2025-07-25 11:45:15 +0900

    ファイル構成を変更

--- src/coding-nikki/httpであそぼう/code/src/http_util/response/mod.rs
+++ src/coding-nikki/httpであそぼう/code/src/http_util/response/mod.rs
@@ -1,5 +1,7 @@
 use super::*;
 use std::{collections::HashMap, fmt};
+#[cfg(test)]
+mod tests;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct HttpResponse<'a> {
@@ -62,26 +64,5 @@ impl<'a> fmt::Display for HttpResponse<'a> {
             write!(f, "{}: {}\r\n", k, v)?;
         }
         write!(f, "\r\n{}", self.body)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn success_response() {
-        let response = r#"HTTP/1.1 200 OK"#;
-        let response = HttpResponse::from_str(response).unwrap();
-        assert_eq!(response.version, HttpVersion::Http11);
-        assert_eq!(response.status, (200, "OK"));
-        assert!(response.header.is_empty());
-        assert!(response.body.is_empty());
-    }
-
-    #[test]
-    fn failure_response() {
-        let response = HttpResponse::from_str("");
-        assert!(response.is_none());
     }
 }
